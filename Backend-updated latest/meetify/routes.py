b8e9b4e8b54bca21a841288a14b7aa199206141a
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2020-05-25 19:06:36 +0530

    Add files via upload

--- Backend-updated latest/meetify/routes.py
+++ Backend-updated latest/meetify/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from meetify import app,db,bcrypt
-from meetify.forms import RegistrationForm, LoginForm
+from meetify.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from meetify.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -12,30 +12,35 @@ def home():
 
 
 @app.route('/login',methods=['GET','POST'])
-def login():    
-	form = LoginForm()
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('account'))
+	
+	form = LoginForm()	
 	if form.validate_on_submit():
 		user=User.query.filter_by(email=form.email.data).first()
 		if user and bcrypt.check_password_hash(user.password,form.password.data):
 			login_user(user)
-			return redirect(url_for('home'))
-
-	else:
-		flash('please check your credentials')
-		return render_template('login.html', form =form)
+			return redirect(url_for('account'))
+		else:
+			flash('please check your credentials', 'danger')
+	return render_template('login.html', form =form)
 
 
 @app.route('/account')
+@login_required
 def account():
-    image_file=url_for('static',filename='default.png')
-    return render_template('account.html')
+	form=UpdateAccountForm()
+	image_file=url_for('static',filename='profilepics/'+current_user.image_file)
+	return render_template('myprofile.html',title='Personal Dashboard',image_file=image_file,form=form)
 
 
 @app.route('/signup',methods=['GET','POST'])
-def signup():
+def signup():	
 	form = RegistrationForm()
+	
+	
 	if form.validate_on_submit():
-
 		hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 		user1=User(username=form.username.data,email=form.email.data,password=hashed_password)
 		db.session.add(user1)
@@ -48,4 +53,10 @@ def signup():
 
 @app.route('/forgot_password')
 def forgot_password():
-    return render_template('forgot_password.html')
+	return render_template('forgot_password.html')
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
