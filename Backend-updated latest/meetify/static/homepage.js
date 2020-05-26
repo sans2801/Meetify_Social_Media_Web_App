386bac58c4b24f40641b05b726d7c0b694bee544
AnkitJaiswal26 <64367253+AnkitJaiswal26@users.noreply.github.com>
2020-05-26 17:08:17 +0530

    Add files via upload

--- Backend-updated latest/meetify/static/homepage.js
+++ Backend-updated latest/meetify/static/homepage.js
@@ -1,1 +1,18 @@
-alert("working");
+const darkSwitch = document.getElementById('darkSwitch');
+window.addEventListener('load', () => {
+  if (darkSwitch) {
+    darkSwitch.addEventListener('change', () => {
+      resetTheme();
+    });
+  }
+});
+
+function resetTheme() {
+  if (darkSwitch.checked) {
+    document.body.setAttribute('data-theme', 'dark');
+    
+  } else {
+    document.body.removeAttribute('data-theme');
+    
+  }
+}
